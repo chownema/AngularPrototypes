ac9f342030dae3368b43cf04e072a3516cc6f9b5
Miguel Saavedra <Miguel Saavedra>
2017-04-12 14:51:19 +1200

    getKeyByValue function done

--- Angular1.511/DeviceManagerProto/class-build-helpers/dictionary.class.js
+++ Angular1.511/DeviceManagerProto/class-build-helpers/dictionary.class.js
@@ -19,14 +19,89 @@
             var Dictionary = function (oDictionary) {
                 var dict = this;
                 
-                dict.data = oDictionary;
+                var properties = {
+                    data : oDictionary,
+                    hasNext : false,
+                    hasPrev : false
+                };
+
+                /**
+                 * @function _searchDictionary
+                 * @param {function} cb callback function
+                 * @param {Dict} args arguments
+                 * @description internal function used to iterate through 
+                 *              the dictionary, whilst implementing a
+                 *              callback function which its result is returned
+                 *              back to the calling function.
+                 * @return      the callback functions result
+                 */
+                function _searchDictionary(cb, args) {
+                    var result = null;
+
+                    if (cb === null) {
+                        throw 'Need to Specify Callback Function';    
+                    }
+
+                    if (properties.data != null) {
+                        keys = Object.keys(properties.data);
+                        // Iterate dictionary data till find key
+                        for (i = 0; i < keys.length; i++) {
+                            // Give current element, Elements List, index
+                            result = cb(keys[i], keys, i);
+                            // if result is found
+                            if (result != null)
+                                break;
+                        }
+                    }
+                    return result;
+                }
+
+                // /**
+                //  * @function getKeyByValue
+                //  * @description Function Desc
+                //  * @return key associated with the value in the dictionary
+                //  *         collection, if not found then return null
+                //  */
+                // dict.getKeyByValue = function (value) {
+                //     // get Keys
+                //     var keys = [];
+                //     var returnKey = null;
+                //     if (properties.data != null) {
+                //         keys = Object.keys(properties.data);
+                //         // Iterate till find key
+                //         for (i = 0; i > keys.length; i++) {
+                //             // If found set returnKey and exit loop
+                //             if(properties.data[keys[i]]) {
+                //                 returnKey = properties.data[keys[i]];
+                //                 break;
+                //             } 
+                //         }
+                //     }
+                //     return returnKey;
+                // };
 
                 /**
                  * @function getKeyByValue
                  * @description Function Desc
+                 * @return key associated with the value in the dictionary
+                 *         collection, if not found then return null
                  */
-                dict.getKeyByValue = function () {
-                    // do something useful
+                dict.getKeyByValue = function (value) {
+                    // get Keys
+                    var keys = [];
+                    // var returnKey = null;
+                    // iterate through dict
+                    var returnKey = _searchDictionary(
+                        function (currentItem, Items, index) {
+                            // If found set returnKey and exit loop
+                            if(properties.data[currentItem]) {
+                                // return it to the result
+                                return properties.data[currentItem];
+                            }
+                        },
+                        null
+                    );
+                    return returnKey;
                 };
 
                 /**
